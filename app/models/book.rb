38f17d5663fc228c3168728fda7828ccdc5066ff
nataze <onataze@gmail.com>
2015-10-22 22:26:57 +0000

    Added method to find book by isbn

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
+require 'httparty'
 class Book < ActiveRecord::Base
   belongs_to :user
   has_one :lend, dependent: :destroy
@@ -6,5 +7,24 @@ class Book < ActiveRecord::Base
   validates :author, presence: true
   validates :isbn, presence: true
   validates :description, presence: true
+
+
+  def self.find_book_with_isbn(isbn)
+    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}")
+     title = response["items"][0]["volumeInfo"]["title"]
+     author = response["items"][0]["volumeInfo"]["authors"][0]
+     description = response["items"][0]["volumeInfo"]["description"]
+     picture_url = nil
+
+     unless response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"].nil?
+        picture_url = response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+     end
+
+    if picture_url.nil?
+      { title: title, author: author, description: description }
+    else
+      { title: title, author: author, description: description, picture_url: picture_url }
+    end
+  end
 
 end
