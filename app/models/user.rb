5630710950ed952bc2b415037724dbeac840f9f2
Toto <naoki3355@gmail.com>
2015-10-23 18:01:24 +0000

    shelves add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  extend Geocoder::Model::ActiveRecord
+  # extend Geocoder::Model::ActiveRecord
  
   has_many :books, dependent: :destroy
   has_many :lends
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
   
 
-  geocoded_by :address
-  reverse_geocoded_by :latitude, :longitude
-  after_validation :geocode, :if => :address_changed?
+  # geocoded_by :address
+  # reverse_geocoded_by :latitude, :longitude
+  # after_validation :geocode, :if => :address_changed?
 end
